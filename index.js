8d36a3a24d28422c0c2b2cc0e662d38d0a11ee93
abhishek upadhyay <abhishekupadhyay483@gmail.com>
2020-11-13 08:46:17 +0530

    remove

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 8001;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const env=require('./config/environment');
-const logger=require('morgan')
+const logger=require('morgan');
+const cors = require('cors');
+
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
@@ -93,6 +95,7 @@ app.use(customMware.setFlash);
 
 // use express router
 app.use('/', require('./routes'));
+
 
 
 app.listen(port, function(err){
